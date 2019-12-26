72cbb82d8b39d28f1d6510a0a2388775c90db258
Tharwat Abouhelal <tharwat96@gmail.com>
2019-12-26 17:43:33 +0200

    clearing branch

---
+++ lab07 - java DB connection/src/form_db_viewer/JavaDBConnection.java
@@ -0,0 +1,11 @@
+package form_db_viewer;
+
+public class JavaDBConnection {
+    private DBGUI gui; 
+    private DBManager dbMan;
+    
+    
+    public static void main(String args[]) {    }
+    
+
+}
